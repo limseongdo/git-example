afe4c4fa2d36c743d2110fc621370b1e4e14c7d8
limseongdo <limseongdo77@gmail.com>
2025-03-17 21:19:18 +0900

    깃 세팅하기

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,19 @@
+package org.example;
+
+
+
+// 문제 : `안녕하세요.`를 10번 출력해주세요.
+// 조건 : System.out.println은 딱 한번만 사용 가능
+// 조건 : \를 활용해서 줄바꿈 해주세요.
+// 힌트 : \n 은 줄바꿈을 의미합니다.
+
+class Main {
+    public static void main(String[] args) {
+        // `\` <= 백스페이스 좌측 키
+        System.out.println("안녕하세요.\n안녕하세요.\n안녕하세요.\n안녕하세요.\n안녕하세요.\n안녕하세요.\n안녕하세요.\n안녕하세요.\n안녕하세요.\n안녕하세요.\n");
+        System.out.println(123);
+
+        System.out.printf("%d", 123);
+    }
+}
+
